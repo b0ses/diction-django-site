9ea498349d1e3bd178ec7f67255d8e87a6133133
b0ses <b0ses@users.noreply.github.com>
2017-12-16 18:29:32 -0500

    fixed urls for absolute paths

--- diction_django_site/urls.py
+++ diction_django_site/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('twisters.urls')),
+    url(r'^', include('diction_django_site.twisters.urls')),
 ]
